7b13cc15486a70b84cebbd2d82708b002e7baa7b
cassiopereira.nunes@gmail.com <cassiopereira.nunes@gmail.com>
2020-03-25 23:23:45 +0000

    Initital version

---
+++ src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/BasicSpringBootRestWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cpn.spring.rest.webservice.basicSpringBootRESTWebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringBootRestWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringBootRestWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/BasicSpringBootRestWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpn.spring.rest.webservice.basicSpringBootRESTWebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootRestWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
