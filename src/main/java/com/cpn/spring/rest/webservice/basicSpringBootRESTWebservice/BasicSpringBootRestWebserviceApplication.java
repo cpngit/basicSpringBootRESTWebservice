83deecd64089c2760d2821796c762f1f6604abc9
cassionunes@cofcointernational.com <cassionunes@cofcointernational.com>
2020-04-04 19:46:08 +0100

    Adding Hateoas

--- src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/BasicSpringBootRestWebserviceApplication.java
+++ src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/BasicSpringBootRestWebserviceApplication.java
@@ -2,12 +2,24 @@ package com.cpn.spring.rest.webservice.basicSpringBootRESTWebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class BasicSpringBootRestWebserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicSpringBootRestWebserviceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
--- src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/helloworld/HelloWordController.java
+++ src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/helloworld/HelloWordController.java
@@ -1,11 +1,17 @@
 package com.cpn.spring.rest.webservice.basicSpringBootRESTWebservice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -20,5 +26,10 @@ public class HelloWordController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWordBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWordBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
