d2178a10ab4aea0dddb353e4a4ebc5cbcd76e7a9
cassionunes@cofcointernational.com <cassionunes@cofcointernational.com>
2020-04-05 16:12:43 +0100

    Added swagger

--- src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/helloworld/HelloWordController.java
+++ src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/helloworld/HelloWordController.java
@@ -1,6 +1,5 @@
 package com.cpn.spring.rest.webservice.basicSpringBootRESTWebservice.helloworld;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWordController {
 
-    @Autowired
-    private MessageSource messageSource;
+    private final MessageSource messageSource;
+
+    public HelloWordController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
