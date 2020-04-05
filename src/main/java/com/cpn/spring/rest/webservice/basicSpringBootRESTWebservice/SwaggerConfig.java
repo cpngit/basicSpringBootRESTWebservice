415a677547f1b2a5b168bc1ce01a3ec706e88ccd
cassionunes@cofcointernational.com <cassionunes@cofcointernational.com>
2020-04-05 16:33:01 +0100

    Added swagger

--- src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/SwaggerConfig.java
+++ src/main/java/com/cpn/spring/rest/webservice/basicSpringBootRESTWebservice/SwaggerConfig.java
@@ -7,20 +7,24 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+            new HashSet<String>(Arrays.asList("application/json", "application/xml"));
+
     @Bean
     public Docket api(){
-       return new Docket(DocumentationType.SWAGGER_2);
+       return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT)
+               .produces(DEFAULT_PRODUCES_AND_CONSUMES).consumes(DEFAULT_PRODUCES_AND_CONSUMES);
    }
 
     @Primary
